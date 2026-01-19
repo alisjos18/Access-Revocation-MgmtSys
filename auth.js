a4619ccc31b93b664376b0e3db1e3765b9d1bdb7
Apurva Joseph <111975071+alisjos18@users.noreply.github.com>
2026-01-19 11:22:15 +0530

    minor fixes

--- auth.js
+++ auth.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./user');
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
     const { username, password, role } = req.body;
-    const hashed = await bcrypt.hash(password, 8);
-    const user = new User({ username, password: hashed, role });
+    const user = new User({ username, password: password, role });
     await user.save();
     res.send({ msg: "done" });
 });
 
 router.post('/login', async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
-    if (!user || !await bcrypt.compare(req.body.password, user.password)) {
+    if (!user || req.body.password !== user.password) {
         return res.status(401).send("fail");
     }
     const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET);
@@ -23,3 +21,4 @@ router.post('/login', async (req, res) => {
 
 
 module.exports = router;
+
