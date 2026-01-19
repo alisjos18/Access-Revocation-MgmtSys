d349baea86eac0d523bd4c645e4c307535cc7da9
Apurva Joseph <111975071+alisjos18@users.noreply.github.com>
2026-01-19 10:39:11 +0530

    reformat schema

--- user.js
+++ user.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['USER', 'ADMIN'], default: 'USER' },
+    role: { type: String, enum: ["USER", "ADMIN"], default: "USER" },
     hasAccess: { type: Boolean, default: true }
 });
-module.exports = mongoose.model('User', UserSchema);
+
+module.exports = mongoose.model("User", UserSchema);
